bacf44f9bfd65b61af97ce5fbd3f27650b4e1ac6
ValentinSirbu <valentinsirbu@yandex.com>
2016-03-10 21:31:16 +0200

    Update Test

--- tests/TestFileUpload.php
+++ tests/TestFileUpload.php
@@ -1,6 +1,14 @@
 <?php
-use Illuminate\Foundation\Testing\TestCase;
-class FileUploadTest extends PHPUnit_Framework_TestCase
+//use Illuminate\Foundation\Testing\TestCase;
+use TestCase;
+class FileUploadTest extends TestCase
 {
-
+    public function testRoutes()
+    {
+        $path = config('fileupload.route_prefix').config('fileupload.route_name');
+        $this->json('POST', '/'.$path, ['name' => 'Test'])
+            ->seeJson([
+                'created' => true,
+            ]);
+    }
 }
