ee853b11a448e34b49e1e77b5a7322e49c875488
ValentinSirbu <valentinsirbu@yandex.com>
2016-03-29 13:45:46 +0300

    Working on testing

--- tests/TestFileUpload.php
+++ tests/TestFileUpload.php
@@ -1,8 +1,10 @@
 <?php
-
+use Illuminate\Foundation\Testing\WithoutMiddleware;
 
 class FileUploadTest extends TestCase
 {
+ use WithoutMiddleware;
+
     public function testRoutes()
     {
         $path = config('fileupload.route_prefix').'/'.config('fileupload.route_name');
@@ -12,4 +14,14 @@ class FileUploadTest extends TestCase
         $this->assertEquals(200, $response_delete->getStatusCode());
 
     }
+
+//    public function testFileUpload()
+//    {
+//        $path = config('fileupload.route_prefix').'/'.config('fileupload.route_name');
+//        $file_path =__DIR__.'\laravel.png';
+//        $uploadedFile = new Symfony\Component\HttpFoundation\File\UploadedFile($file_path, 'laravel.png','image/png', 446, null, null, FALSE );
+//        $response = $this->call('POST', $path, [],[], ['file' => $uploadedFile]);
+//        echo($uploadedFile);
+//        $this->assertEquals(200, $response->getStatusCode());
+//    }
 }
